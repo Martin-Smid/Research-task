fixed LHO solver both for no units and units.
Running gravity potential simulation for orbital motion seems to work

main.py
@@ -13,13 +13,13 @@ N = 512 # Number of spatial points
 
 sim = Simulation_Class(
     dim=1,                             # 2D simulation
-    boundaries=[(-10, 10)], # Spatial boundaries
+    boundaries=[(-40, 40)], # Spatial boundaries
     N=512,                             # Grid resolution
     total_time=2.0,                   # Total simulation time
     h=0.01,                            # Time step
     use_gravity=False,  # Enable gravitational effects
     static_potential=quadratic_potential,
-    use_units=False,
+    use_units=True,
 )
 
 vlna = Wave_function(
@@ -52,11 +52,11 @@ x_vals = np.linspace(a, b, N, endpoint=False)
 
 #controlled_times = [0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10]
 for time in sim.accessible_times:
-    print(f"Time: {time}")
-    print(f"Psi: {vlna.psi  * cp.exp(-1j * energy_nd([0], omega=1, hbar=1) * time)}")
-    an_psi = asnumpy(cp.abs(vlna.psi  * cp.exp(-1j * energy_nd([0], omega=1, hbar=1) * time))**2)
-    an_psi_real = asnumpy(cp.real(vlna.psi  * cp.exp(-1j * energy_nd([0], omega=1, hbar=1) * time)))
-    an_psi_imag = asnumpy(cp.imag(vlna.psi  * cp.exp(-1j * energy_nd([0], omega=1, hbar=1) * time)))
+
+
+    an_psi = asnumpy(cp.abs(sim.get_wave_function_at_time(0)  * cp.exp(-1j * energy_nd([0], omega=1, hbar=sim.h_bar) * time))**2)
+    an_psi_real = asnumpy(cp.real(sim.get_wave_function_at_time(0)  * cp.exp(-1j * energy_nd([0], omega=1, hbar=sim.h_bar) * time)))
+    an_psi_imag = asnumpy(cp.imag(sim.get_wave_function_at_time(0)  * cp.exp(-1j * energy_nd([0], omega=1, hbar=sim.h_bar) * time)))
 
     num_psi = sim.get_wave_function_at_time(time)
     num_psi = asnumpy(num_psi)
@@ -71,8 +71,10 @@ for time in sim.accessible_times:
 plt.legend(['Analytical real', 'Numerical real', 'Analytical imag', 'Numerical imag'], loc='upper right')
 plt.show()
 
-print(sim.wave_values[0])
-print(sim.get_wave_function_at_time(0.4))
+
+print("#--------------------------------------------------------------------------#")
+
+print(sim.get_wave_function_at_time(0))
 print("#--------------------------------------------------------------------------#")
 
 '''

resources/Functions/Schrodinger_eq_functions.py
@@ -30,15 +30,13 @@ def quadratic_potential(packet_class_instance):
         cp.ndarray: The potential computed on the spatial grid.
     """
     grids = packet_class_instance.grids  # Grids (already meshgrids) from the wave_function instance
-
-    mass = 1 # maybe change this later, im tired
     dim = packet_class_instance.dim
     omega = packet_class_instance.total_omega
 
     # Compute r^2 from the multidimensional grid
     r2 = sum(g ** 2 for g in grids)
     # Return the computed quadratic potential
-    return  0.5 * mass * omega ** 2 * r2
+    return  0.5 *  omega ** 2 * r2
 
 
 def gravity_potential(simulation_instance, mass_multiplier=1, center=None):
@@ -82,10 +80,10 @@ def gravity_potential(simulation_instance, mass_multiplier=1, center=None):
 
 
 
-def coefficient_nd(n, m=1, omega=1, hbar=1):
+def coefficient_nd(n, omega=1, h_bar_tilde=1):
     """Calculate the normalization constant for N dimensions."""
 
-    beta = np.sqrt(m * omega / hbar)
+    beta = np.sqrt( omega / h_bar_tilde)
     factor = (beta ** 2 / np.pi) ** (len(n) / 2)  # Normalization factor for N dimensions
     denom = np.sqrt(np.prod([2 ** ni * factorial(ni) for ni in n]))  # Hermite polynomial normalization
     return factor / denom
@@ -105,8 +103,9 @@ def energy_nd(n, omega=1, hbar=1):
     """
     if not isinstance(n, list) or not all(isinstance(x, int) and x >= 0 for x in n):
         raise ValueError("n must be a list of non-negative integers representing quantum numbers.")
+    print(hbar)
 
-    return hbar * omega * (sum(n) + len(n) * 0.5)
+    return 1 * omega * (sum(n) + len(n) * 0.5)
 
 
 def lin_harmonic_oscillator(simulation_instance):
@@ -124,12 +123,12 @@ def lin_harmonic_oscillator(simulation_instance):
     dim = simulation_instance.dim
     means = simulation_instance.means
     dx = simulation_instance.dx
-    mass = 1
     omega = simulation_instance.omega
-    h_bar = 1
+    h_bar_tilde = simulation_instance.h_bar_tilde
+    print(h_bar_tilde)
 
     quantum_numbers = [0] * dim  # Quantum numbers for each dimension
-    beta = np.sqrt((mass * omega) / h_bar)
+    beta = np.sqrt(( omega) / h_bar_tilde)
 
     gaussian_factors = []
     hermite_polynomials = []
@@ -147,7 +146,7 @@ def lin_harmonic_oscillator(simulation_instance):
         psi_0 *= gaussian_factors[i] * hermite_polynomials[i]
 
     # Call coefficient_nd for normalization constant across dimensions
-    coeff = coefficient_nd(quantum_numbers, mass, omega, h_bar)
+    coeff = coefficient_nd(quantum_numbers, omega, h_bar_tilde)
     psi_0 *= coeff  # Apply the normalization constant to psi_0
 
     # Normalize the wavefunction using the provided normalize_wavefunction function